c9cdd665f5bd1f5a179c8514f6e8188db9d5a744
Jae Hyun Goh <skrksbry@naver.com>
2024-04-02 10:13:36 +0900

    fix: 타이틀 통일성 유지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kirsi",
+  title: "KIRSI BLOG",
   description: "Kirsi Tech",
 };
 
