1f5afcaa08a3197eaf65e190eb987edfee24aa15
Jae Hyun Goh <skrksbry@naver.com>
2024-04-06 17:41:28 +0900

    fix: 포스트 읽기 모바일 대응 패치 feat: 신규 블록 컴포넌트 적용 및 환경변수 적용

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -10,15 +10,18 @@ import {IComment} from "@/interface/commentInterface";
 import {getDate} from "@/common/common";
 import {Suspense} from "react";
 import Loading from "@/components/common/Loading";
-import {ImageBlock, TextBlock} from "@/components/view/PostBlocks";
+import {ImageBlock, LinkBlock, TextBlock, HeaderImageBLock} from "@/components/view/PostBlocks";
+import AlertLine from "@/components/view/AlertLine";
+import {UserCircleIcon} from "@heroicons/react/24/solid";
+import CodeBlocks from "@/components/view/CodeBlocks";
 
 const getPostBlocks = async (id:string) => {
-    const res = await fetch(`http://127.0.0.1:4000/posts/${id}`,{ next: { revalidate: 10 } });
+    const res = await fetch(`${process.env.baseUrl}/posts/${id}`,{ next: { revalidate: 10 } });
     return await res.json();
 }
 
 const getPostComments = async (id:string) => {
-    const res = await fetch(`http://127.0.0.1:4000/comments/${id}`,{ cache: 'no-store' })
+    const res = await fetch(`${process.env.baseUrl}/comments/${id}`,{ cache: 'no-store' })
     return await res.json();
 }
 
@@ -38,13 +41,22 @@ const ViewPost = async ({params}:{ params:{id:string}}) => {
         notFound()
     }
 
-    const getRightBlock = (data: IBlock) => {
+    const getBlock = (data: IBlock) => {
         switch (data.block_type) {
             case "img": {
                 return <ImageBlock key={data.block_id} data={data} />
             }
             case "text": {
                 return <TextBlock key={data.block_id} data={data} />
+            }
+            case "link": {
+                return <LinkBlock key={data.block_id} data={data} />
+            }
+            case "code": {
+                return <CodeBlocks key={data.block_id} data={data} />
+            }
+            case "header": {
+                return <HeaderImageBLock key={data.block_id} data={data} />
             }
             default: {
                 return <TextBlock key={data.block_id} data={data} />
@@ -53,7 +65,7 @@ const ViewPost = async ({params}:{ params:{id:string}}) => {
     }
     return (
         <div className="w-full h-full min-h-[100vh] relative flex flex-wrap content-start">
-            <div className="flex relative w-[1024px] m-auto pt-24 pb-12">
+            <div className="flex relative w-full m-0 px-4 lg:w-[1024px] lg:m-auto lg:px-0 pt-24 pb-12">
                 {/*    Content Area   */}
                 <div className="flex flex-1 relative flex-wrap content-start">
                     <div className="left-0 top-[0px] relative">
@@ -63,46 +75,52 @@ const ViewPost = async ({params}:{ params:{id:string}}) => {
                     </div>
                     <div className="flex w-full relative mt-6 flex-col">
                         {postBlocks.blocks.map((data: IBlock)=> (
-                            getRightBlock(data)
+                            getBlock(data)
                         ))}
-                        <div className="flex w-full m-auto pt-4 pb-12 flex-col">
-                            <div className="w-full text-2xl font-bold">이어서보기</div>
-                            <Suspense fallback={<Loading/>}>
-                                {/*<ContinuePost/>*/}
-                            </Suspense>
-                        </div>
-                        <span className="w-full text-xl font-bold mt-4">댓글이 2개 있습니다</span>
-                        <ul role="list" className="pr-12 pt-3">
-                            {postComments.map((data: IComment)=> (
-                                <li key={data.comment_id}
-                                    className="flex w-full py-6 gap-2 flex-wrap border-b-[1px] last:border-b-[0px] border-[rgba(0,0,0,0.3)] dark:border-[rgba(255,255,255,0.3)]">
-                                    <div className="flex w-full flex-wrap gap-3.5 items-center">
-                                        <div className="flex w-12">
-                                            <Image src={gal} alt="comment profile image"
-                                                   className="w-12 h-12 object-cover rounded-full"/>
-                                        </div>
-                                        <div className="flex-wrap flex flex-1 content-center">
-                                            <span className="flex text-[14px] font-bold">{data.comment_owner}</span>
-                                            <span
-                                                className="flex text-[14px] font-light opacity-40 items-center ml-0.5">#F32B</span>
-                                            <span className="w-full text-[9px] text-gray-400">{getDate(data.comment_time)}</span>
-                                        </div>
-                                    </div>
-                                    <div className="flex w-full px-2 py-2">
-                                        <span className="w-full text-[14px]">{data.comment_content}</span>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <div className="flex w-full pr-12 flex-wrap gap-3 justify-end ">
-                            <textarea
-                                placeholder="내용을 입력해 주세요."
-                                className="flex w-full min-h-32 bg-[rgba(0,0,0,0.1)] dark:bg-[rgba(255,255,255,0.1)] outline-none border-none border-[rgba(0,0,0,0.2)] dark:border-[rgba(255,255,255,0.2)] rounded-xl p-4 resize-none"/>
-                            <button
-                                className="flex px-4 py-2 bg-[rgba(0,0,0,0.8)] dark:bg-[rgba(255,255,255,0.8)] text-white dark:text-black rounded-xl justify-center items-center font-bold hover:scale-105 transition-transform active:scale-100">
-                                댓글 등록
-                            </button>
-                        </div>
+                        {/*<div className="flex w-full m-auto pt-4 pb-12 flex-col">*/}
+                        {/*    <div className="w-full text-2xl font-bold">이어서보기</div>*/}
+                        {/*    <Suspense fallback={<Loading/>}>*/}
+                        {/*        /!*<ContinuePost/>*!/*/}
+                        {/*    </Suspense>*/}
+                        {/*</div>*/}
+                        { postBlocks.post_hidden ? <AlertLine content={`이 포스트는 링크로만 접근 가능한 비공개 포스트입니다.`}  level={0}/>
+                        :
+                            <>
+                                <span className="w-full text-xl font-bold mt-4">댓글이 {postComments.length}개 있습니다</span>
+                                <ul role="list" className="pr-12 pt-3">
+                                    {postComments.map((data: IComment) => (
+                                        <li key={data.comment_id}
+                                            className="flex w-full py-6 gap-2 flex-wrap border-b-[1px] last:border-b-[0px] border-[rgba(0,0,0,0.3)] dark:border-[rgba(255,255,255,0.3)]">
+                                            <div className="flex w-full flex-wrap gap-3.5 items-center">
+                                                <div className="flex w-12">
+                                                    <UserCircleIcon className="w-12 h-12"/>
+                                                </div>
+                                                <div className="flex-wrap flex flex-1 content-center">
+                                                    <span
+                                                        className="flex text-[14px] font-bold">{data.comment_owner}</span>
+                                                    <span
+                                                        className="flex text-[14px] font-light opacity-40 items-center ml-0.5">#F32B</span>
+                                                    <span
+                                                        className="w-full text-[9px] text-gray-400">{getDate(data.comment_time)}</span>
+                                                </div>
+                                            </div>
+                                            <div className="flex w-full px-2 py-2">
+                                                <span className="w-full text-[14px]">{data.comment_content}</span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <div className="flex w-full flex-wrap gap-3 justify-end ">
+                                    <textarea
+                                        placeholder="내용을 입력해 주세요."
+                                        className="flex w-full min-h-32 bg-[rgba(0,0,0,0.1)] dark:bg-[rgba(255,255,255,0.1)] outline-none border-none border-[rgba(0,0,0,0.2)] dark:border-[rgba(255,255,255,0.2)] rounded-xl p-4 resize-none"/>
+                                    <button
+                                        className="flex px-4 py-2 bg-[rgba(0,0,0,0.8)] dark:bg-[rgba(255,255,255,0.8)] text-white dark:text-black rounded-xl justify-center items-center font-bold hover:scale-105 transition-transform active:scale-100">
+                                        댓글 등록
+                                    </button>
+                                </div>
+                            </>
+                        }
                     </div>
                 </div>
                 <PostIndicator blocks={postBlocks.blocks}/>
--- src/components/view/PostBlocks.tsx
+++ src/components/view/PostBlocks.tsx
@@ -1,5 +1,12 @@
 import {IBlock} from "@/interface/postInterface";
 import Image from "next/image";
+import {LinkIcon} from "@heroicons/react/20/solid";
+import Link from "next/link";
+
+const getPostBlocks = async (id:string) => {
+    const res = await fetch(`${process.env.baseUrl}/posts/${id}`,{ next: { revalidate: 10 } });
+    return await res.json();
+}
 
 const TextBlock = ({data}: {data: IBlock}) => {
     return (
@@ -14,12 +21,13 @@ const TextBlock = ({data}: {data: IBlock}) => {
 const ImageBlock = ({data}: {data: IBlock}) => {
     return (
         <div key={`content-${data.block_id}`} id={`content-${data.block_id}`}
-             className="w-full flex justify-center my-4 relative">
-            <div className="h-auto flex image-cover" style={{width: data.block_option === "F" ? "100%" : "50%"}}>
+             className="w-full flex justify-center my-4" style={{position: 'relative'}}>
+            <div className="h-auto flex image-cover" style={{width: data.block_option === "F" ? "100%" : "50%", position: 'relative'}}>
                 <Image
                     src={data.block_content}
                     alt={"본문이미지"}
                     fill
+                    sizes="(max-width: 768px) 100vw, 100vw"
                     className="object-contain flex image-unset"
                 />
             </div>
@@ -27,4 +35,40 @@ const ImageBlock = ({data}: {data: IBlock}) => {
     )
 }
 
-export {TextBlock, ImageBlock}
+const HeaderImageBLock = ({data}: {data: IBlock}) => {
+    return (
+        <div key={`content-${data.block_id}`} id={`content-${data.block_id}`}
+             className="w-full flex justify-center my-4" style={{position: 'relative'}}>
+            <div className="h-auto flex image-cover" style={{width: "100%", background: data.block_option, position: 'relative'}}>
+                <Image
+                    src={data.block_content}
+                    alt={"본문이미지"}
+                    fill
+                    sizes="(max-width: 768px) 100%, 100%"
+                    className="object-contain flex image-unset"
+                />
+            </div>
+        </div>
+    )
+}
+
+const LinkBlock = async ({data}: {data: IBlock}) => {
+    if(data.block_content === "blog_link") {
+        const postInformation = await getPostBlocks(data.block_option);
+        return (
+            <Link target={"_blank"} href={`/view/${data.block_option}`} key={`content-${data.block_id}`}
+                  id={`content-${data.block_id}`}
+                  className="bg-gray-400 dark:bg-gray-800 p-2 my-2 rounded-md relative flex items-center gap-2 cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-700"><LinkIcon
+                width={18} height={18}/>{postInformation.post_name}</Link>
+        )
+    } else {
+        return (
+            <Link target={"_blank"} href={`${data.block_option}`} key={`content-${data.block_id}`}
+                  id={`content-${data.block_id}`}
+                  className="bg-gray-400 dark:bg-gray-800 p-2 my-2 rounded-md relative flex items-center gap-2 cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-700"><LinkIcon
+                width={18} height={18}/>{data.block_content}</Link>
+        )
+    }
+}
+
+export {TextBlock, ImageBlock, LinkBlock, HeaderImageBLock}
