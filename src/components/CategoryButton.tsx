e1e51ce1d09beaf4ae6dc48bf670c34da8fc3fcc
Jae Hyun Goh <skrksbry@naver.com>
2024-04-06 17:39:32 +0900

    fix: 버튼 hover 스타일 개선

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 const CategoryButton = ({isSelect=false,children,link,onClick =()=>{}} : {isSelect :boolean,children  :React.ReactNode,link: string,onClick:()=>void}) => {
 
     return (
-        <Link href={link.length > 0 ? `/?ct=${link}` : `/`} onClick={onClick} className="py-2 flex flex-wrap items-center gap-0.5 rounded-lg text-[14px] mt-2 cursor-pointer hover:scale-110"
+        <Link href={link.length > 0 ? `/?ct=${link}` : `/`} onClick={onClick} className="py-2 flex flex-wrap items-center opacity-50 gap-0.5 rounded-lg text-[14px] mt-2 cursor-pointer hover:opacity-100"
         style={{
             color: isSelect ? "" : "",
-            opacity: isSelect ? "1" : "0.5",
+            opacity: isSelect ? "1" : "",
             transition: "transform 0.25s, opacity 0.25s"
         }}>
             {children}
