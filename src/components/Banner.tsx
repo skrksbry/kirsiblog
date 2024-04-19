3fc12fe5758ce628b4fd23a5a0221f77870ad130
Jae Hyun Goh <skrksbry@naver.com>
2024-04-20 02:20:55 +0900

    fix: banner select disable

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
 
     return (
         <div
-            className="h-[150px] flex image-cover overflow-hidden justify-center items-center"
+            className="h-[150px] flex image-cover overflow-hidden justify-center items-center pointer-events-none select-none"
             style={{width: "100%", background: "linear-gradient(90deg, #ef4444 0%, rgb(218, 174, 81) 100%)", position: "relative"}}>
             <div className="w-[150px] pt-[50px] absolute">
                 <Image
@@ -12,7 +12,7 @@ const Banner = () => {
                     alt={"본문이미지"}
                     fill
                     sizes="(max-width: 768px) 100%, 100%"
-                    className="object-contain flex image-unset"
+                    className="object-contain flex image-unset pointer-events-none select-none"
                 />
             </div>
             {/*<span>블로그에 오신걸 환영합니다.</span>*/}
