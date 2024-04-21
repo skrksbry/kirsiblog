cfe11d9b39de69042d037f519b7350c5dd8d59bb
Jae Hyun Goh <skrksbry@naver.com>
2024-04-22 04:56:36 +0900

    feat: CardList Title 변경되도록 수정, 위치변경

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,21 +1,50 @@
 'use server'
 import {IPost} from "@/interface/postInterface";
 import PostCard from "@/components/PostCard";
+import {ExclamationTriangleIcon} from "@heroicons/react/24/solid";
 
 const getPosts = async (searchParams: { [key: string]: string | string[] | undefined}) => {
     const requestUrl = searchParams.ct ? `${process.env.baseUrl}/posts/category/${searchParams.ct}` : `${process.env.baseUrl}/posts/`
     const res = await fetch(requestUrl,{ next: { revalidate: 30 } });
     return await res.json();
 }
+
+const getParamMatchName = (searchParams: { [key: string]: string | string[] | undefined}) => {
+    switch (searchParams.ct){
+        case "all": {
+            return "RECENT";
+        }
+        case "tech": {
+            return "TECH";
+        }
+        case "uiux": {
+            return "UI/UX";
+        }
+        case "diary": {
+            return "DIARY";
+        }
+        default : {
+            return "RECENT"
+        }
+    }
+}
 const PostList = async ({searchParams}: { searchParams: { [key: string]: string | string[] | undefined }}) => {
     const posts = await getPosts(searchParams);
     return (
-        <div className="grid grid-cols-1 w-full gap-2 lg:grid-cols-4">
-            {posts.map((post: IPost) => (
-                <PostCard key={post.post_id} post={post} />
-            ))}
-
-        </div>
+        <>
+            <span className="w-full text-xl font-bold mt-4">{getParamMatchName(searchParams)} POST</span>
+            {posts.length === 0 &&
+                <div className="flex w-full mt-12 flex-col justify-center items-center gap-1">
+                    <ExclamationTriangleIcon width={42} height={42}/>
+                    <span className="font-bold text-[12px]">작성된 포스트가 없습니다</span>
+                </div>
+            }
+            <div className="grid grid-cols-1 w-full gap-2 lg:grid-cols-4">
+                {posts.map((post: IPost) => (
+                    <PostCard key={post.post_id} post={post}/>
+                ))}
+            </div>
+        </>
     );
 }
 
