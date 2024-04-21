8d63edbb43880bbeedb056904dd44415b26050c6
Jae Hyun Goh <skrksbry@naver.com>
2024-04-22 03:58:06 +0900

    fix: useEffect import 삭제

--- src/components/view/ContinuePost.tsx
+++ src/components/view/ContinuePost.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {IPost} from "@/interface/postInterface";
 import PostCard from "@/components/PostCard";
 
--- src/components/write/EditBlock.tsx
+++ src/components/write/EditBlock.tsx
@@ -1,6 +1,5 @@
 "use client";
-import {useEffect, useRef, useState} from "react";
-import Image from "next/image";
+import {useRef, useState} from "react";
 
 interface Blocks {
     index: number;
