07602221785427afb773b03ea881c874fe2f9fbc
Jae Hyun Goh <skrksbry@naver.com>
2024-06-10 22:02:19 +0900

    feat: bannder 사이즈 변경, 이미지 -> 스파인 플레이어 컴포넌트로 변경

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import BannerCharView from "@/components/common/BannerCharView";
 
 const Banner = () => {
 
     return (
         <div
-            className="h-[150px] flex image-cover overflow-hidden justify-center items-center select-none rounded-lg"
+            className="h-[200px] flex image-cover overflow-hidden justify-center items-center select-none rounded-lg"
             style={{
                 width: "100%",
                 background: "linear-gradient(90deg, #ef4444 0%, rgb(218, 174, 81) 100%)",
@@ -14,14 +15,8 @@ const Banner = () => {
                 <span>WELCOME</span>
                 <span>MY BLOG</span>
             </div>
-            <div className="w-[150px] pt-[50px] absolute">
-                <Image
-                    src="/sd.png"
-                    alt={"본문이미지"}
-                    fill
-                    sizes="(max-width: 768px) 100%, 100%"
-                    className="object-contain flex image-unset pointer-events-none select-none"
-                />
+            <div className="absolute h-full">
+                    <BannerCharView />
             </div>
             {/*<span>블로그에 오신걸 환영합니다.</span>*/}
         </div>
