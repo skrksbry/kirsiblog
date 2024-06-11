d0f868eaea2c2c425e1b47cbad6967696d96c76f
Jae hyun Goh <skrksbry@naver.com>
2024-06-11 07:18:59 +0000

    feat: header 이미지 추가, 디자인 컨셉 변경

--- src/app/mdview/[id]/page.tsx
+++ src/app/mdview/[id]/page.tsx
@@ -1,4 +1,5 @@
 import MarkdownPostViewer from "@/components/view/MarkdownPostViewer";
+import Image from "next/image";
 
 const getMarkdownPost = async (id:string) => {
     const res = await fetch(`${process.env.baseUrl}/markdown-posts/${id}`,{ next: { revalidate: 10 } });
@@ -15,6 +16,18 @@ const MarkdownPostView = async ({params}:{ params: {id: string}}) => {
                         <h1
                             className="w-full relative text-4xl font-bold text-black dark:text-white">{"TITLE"}</h1>
                         <div className="w-full relative ext-[12px] text-gray-400 mt-1">{"0000-00-00 00:00"}</div>
+                    </div>
+                    <div className="w-full flex justify-center my-4" style={{position: 'relative'}}>
+                        <div className="h-auto flex image-cover rounded-[12px]" style={{width: "100%", background: postContent.post_color, position: 'relative'}}>
+                            <Image
+                                src={postContent.post_image}
+                                alt={"본문이미지"}
+                                fill
+                                sizes="100%, 100%"
+                                className="object-contain flex image-unset"
+                                priority={true}
+                            />
+                        </div>
                     </div>
                     <MarkdownPostViewer md={postContent.post_content}/>
                 </div>
