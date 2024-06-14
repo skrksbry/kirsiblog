4a10c2f154b5972f3c87411c72efbfc56674f7c8
Jae hyun Goh <skrksbry@naver.com>
2024-06-14 08:00:28 +0000

    fix: 마크다운 포스트용으로 변경, 새로만든 컴포넌트로 내용 변경

--- src/components/view/ContinuePost.tsx
+++ src/components/view/ContinuePost.tsx
@@ -1,25 +1,19 @@
-"use client"
 import {useState} from "react";
-import {IPost} from "@/interface/postInterface";
+import {IMarkdownPost, IPost} from "@/interface/postInterface";
 import PostCard from "@/components/PostCard";
+import PrevNextPost from "../PrevNextPost";
 
-const ContinuePost = () => {
-    const [posts,setPost] = useState<IPost[]>([]);
+const getPrevNextPost = async (id:string) => {
+    const res = await fetch(`${process.env.baseUrl}/markdown-posts/prev-next/${id}`,{ next: { revalidate: 10 } });
+    return await res.json();
+}
 
-    const getPost = () => {
-        fetch(`${process.env.baseUrl}/posts/`)
-            .then(async (r) => {
-                const data = await r.json();
-                setPost(data);
-            });
-    }
-    getPost();
-
+const ContinuePost = async ({id}:{id:string}) => {
+    const posts = await getPrevNextPost(id);
     return (
-        <div className="flex w-full gap-2">
-            {posts.map((post: IPost) => (
-                // <PostCard key={post.post_id} post={post} />
-                <></>
+        <div className={`flex w-full gap-2 ${posts.length === 1 && posts[0].post_call_id > id ? "justify-end":""}`}>
+            {posts.map((post: IMarkdownPost) => (
+                <PrevNextPost key={post.post_call_id} post={post} basicPostId={id} />
             ))}
         </div>
     );
