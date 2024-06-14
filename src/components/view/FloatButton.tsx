7b6c2738a3d74b868bf04b484b2d6d48b82946e1
Jae hyun Goh <skrksbry@naver.com>
2024-06-14 08:00:54 +0000

    fix: 색상문제 수정 및 zindex수정

--- src/components/view/FloatButton.tsx
+++ src/components/view/FloatButton.tsx
@@ -58,14 +58,14 @@ const FloatButton = () => {
     },[bounce])
 
     return (
-        <div className="ml-5 flex justify-end items-end gap-2 flex-col fixed right-8 bottom-8">
+        <div className="ml-5 z-20 flex justify-end items-end gap-2 flex-col fixed right-8 bottom-8">
             <div onClick={scrollToTop} ref={bounceRef} className={`flex items-center px-2 py-2 h-8 font-bold rounded-[8px] overflow-hidden hover:bg-red-500 dark:hover:bg-red-500 bg-black dark:bg-white dark:text-black cursor-pointer transition-all duration-500 ${scrollPos > 150 ? "opacity-100" : "opacity-0"}  ${scrollLimitPos < scrollPos ? "w-[110px]" : "w-8"}`}>
                 <ChevronUpIcon className="min-w-4 min-h-4 w-4 h-4 text-white dark:text-black"/>
-                <div className={`flex text-[12px] transition-all overflow-hidden duration-300 text-nowrap items-center ${scrollLimitPos < scrollPos ? "opacity-100" : "opacity-0"}`}>맨 위로 돌아가기</div>
+                <div className={`flex text-[12px] text-white dark:text-black transition-all overflow-hidden duration-300 text-nowrap items-center ${scrollLimitPos < scrollPos ? "opacity-100" : "opacity-0"}`}>맨 위로 돌아가기</div>
             </div>
             <div onClick={goToBack} className={`flex items-center px-2 py-2 h-8 font-bold rounded-[8px] overflow-hidden hover:bg-red-500 dark:hover:bg-red-500 bg-black dark:bg-white dark:text-black cursor-pointer transition-all duration-500 ${scrollPos > 150 ? "opacity-100" : "opacity-0"}  ${scrollLimitPos < scrollPos ? "w-[130px]" : "w-8"}`}>
                 <ChevronLeftIcon className="min-w-4 min-h-4 w-4 h-4 text-white dark:text-black"/>
-                <div className={`flex text-[12px] transition-all overflow-hidden duration-300 text-nowrap items-center ${scrollLimitPos < scrollPos ? "opacity-100" : "opacity-0"}`}>다른 포스트 찾아보기</div>
+                <div className={`flex text-[12px] text-white dark:text-black transition-all overflow-hidden duration-300 text-nowrap items-center ${scrollLimitPos < scrollPos ? "opacity-100" : "opacity-0"}`}>다른 포스트 찾아보기</div>
             </div>
         </div>
     )
