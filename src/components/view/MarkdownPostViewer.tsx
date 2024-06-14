14c99bbd0300c10958a9890703eaf9f09fd9ddbf
Jae Hyun Goh <skrksbry@naver.com>
2024-06-14 20:29:04 +0900

    test: 서버 환경 문제 확인

--- src/components/view/MarkdownPostViewer.tsx
+++ src/components/view/MarkdownPostViewer.tsx
@@ -1,11 +1,14 @@
 "use client"
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
-import MDEditor from "@uiw/react-md-editor";
+import dynamic from "next/dynamic";
+const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
 const MarkdownPostViewer = ({md = ""}:{md: string}) => {
 
+
     return (
+        // @ts-ignore
         <MDEditor.Markdown
             style={{ width: '100%', backgroundColor: 'transparent' }}
             source={md}
