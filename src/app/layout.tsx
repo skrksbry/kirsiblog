4b560856850ad3cfa168c48af082064172ff6d91
Jae hyun Goh <skrksbry@naver.com>
2024-06-18 07:56:36 +0000

    refec: 쓰지않는 import 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import type {Metadata} from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { cookies } from 'next/headers'
 import Header from "@/components/Header";
