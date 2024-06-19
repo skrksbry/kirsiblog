554555fd339d376db8ca9d0419e94495c90a4927
Jae hyun Goh <skrksbry@naver.com>
2024-06-19 10:37:47 +0000

    ref: layer 클래스로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Banner from "@/components/Banner";
 
 const Home = ({searchParams}: { searchParams: { [key: string]: string | string[] | undefined }}) => {
     return (
-        <div className="w-full min-h-full relative flex flex-wrap content-start items-start">
+        <div className="blg-page">
             <div className="flex relative w-full px-6 lg:px-0 lg:w-[1024px] m-auto pt-24 pb-12 flex-wrap gap-6">
                 <Banner />
                 <CategoryMenu searchParams={searchParams}/>
