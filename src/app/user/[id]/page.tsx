4e6c8a98a48e5ad204b3dfaeb3bc3dfb324f3154
Jae hyun Goh <skrksbry@naver.com>
2025-01-10 16:07:05 +0000

    feat: 개인 프로필 디자인 개선, 모바일 지원

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -37,55 +37,64 @@ const UserInformation = async ({ params }: { params: { id: string } }) => {
 				{user ? (
 					<>
 						<div
-							className="w-full mx-4 mb-6 lg:mx-0 lg:w-auto h-[340px] rounded-[8px] flex items-center flex-wrap flex-col"
+							className="w-full mx-4 mb-6 lg:mx-0 lg:mr-1 lg:w-auto h-auto rounded-[8px] min-h-[340px] flex flex-wrap flex-col backdrop-blur-lg"
 							style={{
-								background: '#bb004a',
+								background: 'rgba(187, 0, 74,0.5) ',
 								position: 'relative',
 							}}
 						>
-							<div className="flex w-full px-3 py-3 gap-2 items-center">
-								<Image
-									width={
-										18
-									}
-									height={
-										18
-									}
-									src="https://r2.silvercherry.io/samlogo_only.png"
-									alt={
-										'blog-logo-simple'
-									}
-								/>
-								{/* <Logo fill="#fff" /> */}
-								<span
-									className="font-light"
-									style={{
-										fontFamily: 'TTHakgyoansimMonggeulmonggeulR',
-									}}
-								>
-									Writter
+							<div className="w-[230px] lg:w-full h-[340px] bg-[#bb004a] absolute top-0 left-0 z-0 rounded-[8px]"></div>
+							<div className="w-[230px] lg:w-full z-10 h-[340px] flex flex-wrap items-center flex-col">
+								<div className="flex w-full px-3 py-3 gap-2 items-center z-20">
+									<Image
+										width={
+											18
+										}
+										height={
+											18
+										}
+										src="https://r2.silvercherry.io/samlogo_only.png"
+										alt={
+											'blog-logo-simple'
+										}
+									/>
+									{/* <Logo fill="#fff" /> */}
+									<span
+										className="font-light"
+										style={{
+											fontFamily: 'TTHakgyoansimMonggeulmonggeulR',
+										}}
+									>
+										Writter
+									</span>
+								</div>
+								<div className="w-40 h-[200px] overflow-hidden box-content pl-2 mx-6 my-1 mb-0 z-[4]">
+									<BannerCharView
+										width={
+											400
+										}
+										height={
+											400
+										}
+										y={
+											0
+										}
+										x={
+											-100
+										}
+									/>
+								</div>
+								<div className="w-full h-[2px] z-10 px-8">
+									<div className="w-full h-[2px] bg-white box-content" />
+								</div>
+								<span className="font-semibold text-white text-2xl z-[4]">
+									{userInformation?.nickname.toUpperCase()}
 								</span>
-							</div>
-							<div className="w-40 h-[200px] overflow-hidden box-content pl-2 border-b-2 border-opacity-30 mx-6 my-1 z-[4]">
-								<BannerCharView
-									width={
-										400
-									}
-									height={
-										400
-									}
-									y={0}
-									x={-100}
-								/>
-							</div>
-							<span className="font-semibold text-white text-2xl z-[4]">
-								{userInformation?.nickname.toUpperCase()}
-							</span>
-							<span className="font-light text-[10px] mb-6 text-white z-[4]">
-								Frontend
-								Developer
-							</span>
-							{/* <span className="font-light text-sm mb-6 text-white z-[4]">
+								<span className="font-light text-[10px] mb-6 text-white z-[4]">
+									Frontend
+									Developer
+								</span>
+								{/* <span className="font-light text-sm mb-6 text-white z-[4]">
 								포스트{' '}
 								{
 									userInformation
@@ -94,8 +103,9 @@ const UserInformation = async ({ params }: { params: { id: string } }) => {
 								}
 								개
 							</span> */}
+							</div>
 						</div>
-						<div className="flex flex-1 flex-wrap">
+						<div className="flex flex-1 flex-wrap lg:ml-3 border-l-[0px] border-dotted border-[rgb(187,0,74,0.5)]">
 							<div className="flex w-full rounded-[8px] mx-4 min-h-32 bg-[#bb004a] justify-center items-center">
 								<span>
 									위젯
