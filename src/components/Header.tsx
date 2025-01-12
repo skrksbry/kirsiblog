84911454e120014bc2a52f2829a1f623c018956f
Jae hyun Goh <skrksbry@naver.com>
2025-01-12 12:06:06 +0000

    feat: header 색상 변경 및 blur 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -113,7 +113,7 @@ const Header = ({
 	};
 	return (
 		<div
-			className={`z-20 py-4 px-3 lg:px-4 w-full fixed items-center gap-3 border-b-[1px] bg-[rgba(255,255,255,0.5)] dark:bg-[rgba(0,0,0,0.5)] backdrop-blur-sm left-0 top-0 flex-wrap ${
+			className={`z-20 py-4 px-3 lg:px-4 w-full fixed items-center gap-3 border-b-[1px] bg-[rgba(255,255,255,0.5)] dark:bg-[rgba(0,0,0,0.4)] backdrop-blur-[12px] left-0 top-0 flex-wrap ${
 				scroll > 8 ? 'drop-shadow-sm' : ''
 			}`}
 			style={
